de75fc7f8f90eccac27939d6e6b003941a594520
antonio-bianco <40561025+antonio-bianco@users.noreply.github.com>
2019-01-21 11:47:06 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -1,133 +1,79 @@
-"use strict";
-
-var renderer, scene, camera, composer, circle, skelet, particle;
-
-window.onload = function() {
-  init();
-  animate();
-}
-
-function init() {
-  renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
-  renderer.setPixelRatio((window.devicePixelRatio) ? window.devicePixelRatio : 1);
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  renderer.autoClear = false;
-  renderer.setClearColor(0x000000, 0.0);
-  document.getElementById('canvas').appendChild(renderer.domElement);
-
-  scene = new THREE.Scene();
-
-  camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000);
-  camera.position.z = 210;
-  scene.add(camera);
-
-  particle = new THREE.Object3D();
-  
-  scene.add(particle);
-
-  var geometry = new THREE.TetrahedronGeometry(2, 0);
-  var geom = new THREE.IcosahedronGeometry(7, 1);
-  var geom2 = new THREE.IcosahedronGeometry(15, 1);
-
-  var material = new THREE.MeshPhongMaterial({
-    color: 0xffffff,
-    shading: THREE.FlatShading
+$(document).ready(function () {  
+ $('body > div.container.full-height > div.row.full-content > div.col.s12.m5.l5.white-text > div').addClass('image-animated'); 
+ $('body > div.container.full-height > div.row.full-content-resume > div:nth-child(3) > div').addClass('side-animated'); 
+ $('#toggle').click(function() {
+  $(this).toggleClass('active');
+  $('#overlay').toggleClass('open');
+});
+  $(document).on('keydown', function(e) {
+    if (e.keyCode === 27) { 
+      $("#toggle").removeClass('active');
+      $('#overlay').removeClass('open');
+    }
   });
 
-  for (var i = 0; i < 580; i++) {
-    var mesh = new THREE.Mesh(geometry, material);
-    mesh.position.set(Math.random() - 0.5, Math.random() - 0.5, Math.random() - 0.5).normalize();
-    mesh.position.multiplyScalar(90 + (Math.random() * 700));
-    mesh.rotation.set(Math.random() * 2, Math.random() * 2, Math.random() * 2);
-    particle.add(mesh);
-  }
-  
-  var lights = [];
-  lights[0] = new THREE.DirectionalLight( 0xffffff, 1 );
-  lights[0].position.set( 1, 0, 0 );
-  lights[1] = new THREE.DirectionalLight( 0x66ffff, 1 );
-  lights[1].position.set( 0.75, 1, 0.5 );
-  lights[2] = new THREE.DirectionalLight( 0x66ffff, 1 );
-  lights[2].position.set( -0.75, -1, 0.5 );
-  scene.add( lights[0] );
-  scene.add( lights[1] );
-  scene.add( lights[2] );
-  
-  window.addEventListener('resize', onWindowResize, false);
 
-};
+var top = $('.section-a').offset().top;
+var topb = $('.section-b').offset().top;
+var topc = $('.section-c').offset().top;
+var topd = $('.section-d').offset().top;
 
-function onWindowResize() {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-}
+$(window).scroll(function (event) {
 
-function animate() {
-  requestAnimationFrame(animate);
+  var y = $(this).scrollTop();
 
-  particle.rotation.x += 0.0000;
-  particle.rotation.y -= 0.0020;
-  renderer.clear();
-  renderer.render( scene, camera )
-};
+  if (y >= top){
+   $('div.container.full-height.section-a > div > div > div').addClass('img-animated'); 
+   $('div.container.full-height.section-a > div > div > div').removeClass('img-noanimated');
+   $('div.container.full-height.section-a > div > div > div > div.see-button-wrapper').addClass('see-animated');
+   $('div.container.full-height.section-a > div > div > div > h3').addClass('text-animated');
+   $('div.container.full-height.section-a > div > div > div').removeClass('text-noanimated');
+ }else{
+   $('div.container.full-height.section-a > div > div > div').addClass('img-noanimated');
+   $('div.container.full-height.section-a > div > div > div').removeClass('img-animated');
+   $('div.container.full-height.section-a > div > div > div > a > h6').removeClass('see-animated');
+   $('div.container.full-height.section-a > div > div > div > div.see-button-wrapper').removeClass('see-animated');
+   $('div.container.full-height.section-a > div > div > div').addClass('text-noanimated');
+   $('div.container.full-height.section-a > div > div > div > h3').removeClass('text-animated');
+ }
 
-$(document).ready(function () {  
-   $('body > div.container.full-height > div.row.full-content > div.col.s12.m5.l5.white-text > div').addClass('image-animated'); 
-   $('body > div.container.full-height > div.row.full-content-resume > div:nth-child(3) > div').addClass('side-animated'); 
+ if (y >= topb){
+   $('div.container.full-height.section-b > div > div > div').addClass('img-animated'); 
+   $('div.container.full-height.section-b > div > div > div').removeClass('img-noanimated');
+   $('div.container.full-height.section-b > div > div > div > div.see-button-wrapper').addClass('see-animated');
+   $('div.container.full-height.section-b > div > div > div > h3').addClass('text-animated');
+ }else{
+   $('div.container.full-height.section-b > div > div > div').addClass('img-noanimated');
+   $('div.container.full-height.section-b > div > div > div').removeClass('img-animated');
+   $('div.container.full-height.section-b > div > div > div > div.see-button-wrapper').removeClass('see-animated');
+   $('div.container.full-height.section-b > div > div > div > h3').removeClass('text-animated');
+ }
 
-  $('#toggle').click(function() {
-   $(this).toggleClass('active');
-   $('#overlay').toggleClass('open');
- });
+ if (y >= topc){
+   $('div.container.full-height.section-c > div > div > div').addClass('img-animated'); 
+   $('div.container.full-height.section-c > div > div > div').removeClass('img-noanimated');
+   $('div.container.full-height.section-c > div > div > div > div.see-button-wrapper').addClass('see-animated');
+   $('div.container.full-height.section-c > div > div > div > h3').addClass('text-animated');
+ }else{
+   $('div.container.full-height.section-c > div > div > div').addClass('img-noanimated');
+   $('div.container.full-height.section-c > div > div > div').removeClass('img-animated');
+   $('div.container.full-height.section-c > div > div > div > div.see-button-wrapper').removeClass('see-animated');
+   $('div.container.full-height.section-c > div > div > div > h3').removeClass('text-animated');
+ }
 
-  var top = $('.section-a').offset().top;
-  var topb = $('.section-b').offset().top;
-  var topc = $('.section-c').offset().top;
-  $(window).scroll(function (event) {
+ if (y >= topd){
+   $('div.container.full-height.section-d > div > div > div').addClass('img-animated'); 
+   $('div.container.full-height.section-d > div > div > div').removeClass('img-noanimated');
+   $('div.container.full-height.section-d > div > div > div > div.see-button-wrapper').addClass('see-animated');
+   $('div.container.full-height.section-d > div > div > div > h3').addClass('text-animated');
+ }else{
+   $('div.container.full-height.section-d > div > div > div').addClass('img-noanimated');
+   $('div.container.full-height.section-d > div > div > div').removeClass('img-animated');
+   $('div.container.full-height.section-d > div > div > div > div.see-button-wrapper').removeClass('see-animated');
+   $('div.container.full-height.section-d > div > div > div > h3').removeClass('text-animated');
+ }
 
-    var y = $(this).scrollTop();
-
-    if (y >= top){
-     $('div.container.full-height.section-a > div > div > div').addClass('img-animated'); 
-     $('div.container.full-height.section-a > div > div > div').removeClass('img-noanimated');
-     $('div.container.full-height.section-a > div > div > div > div.see-button-wrapper').addClass('see-animated');
-     $('div.container.full-height.section-a > div > div > div > h3').addClass('text-animated');
-     $('div.container.full-height.section-a > div > div > div').removeClass('text-noanimated');
-   }else{
-     $('div.container.full-height.section-a > div > div > div').addClass('img-noanimated');
-     $('div.container.full-height.section-a > div > div > div').removeClass('img-animated');
-     $('div.container.full-height.section-a > div > div > div > a > h6').removeClass('see-animated');
-     $('div.container.full-height.section-a > div > div > div > div.see-button-wrapper').removeClass('see-animated');
-     $('div.container.full-height.section-a > div > div > div').addClass('text-noanimated');
-     $('div.container.full-height.section-a > div > div > div > h3').removeClass('text-animated');
-   }
-
-   if (y >= topb){
-     $('div.container.full-height.section-b > div > div > div').addClass('img-animated'); 
-     $('div.container.full-height.section-b > div > div > div').removeClass('img-noanimated');
-     $('div.container.full-height.section-b > div > div > div > div.see-button-wrapper').addClass('see-animated');
-     $('div.container.full-height.section-b > div > div > div > h3').addClass('text-animated');
-   }else{
-     $('div.container.full-height.section-b > div > div > div').addClass('img-noanimated');
-     $('div.container.full-height.section-b > div > div > div').removeClass('img-animated');
-     $('div.container.full-height.section-b > div > div > div > div.see-button-wrapper').removeClass('see-animated');
-     $('div.container.full-height.section-b > div > div > div > h3').removeClass('text-animated');
-   }
-
-   if (y >= topc){
-     $('div.container.full-height.section-c > div > div > div').addClass('img-animated'); 
-     $('div.container.full-height.section-c > div > div > div').removeClass('img-noanimated');
-     $('div.container.full-height.section-c > div > div > div > div.see-button-wrapper').addClass('see-animated');
-     $('div.container.full-height.section-c > div > div > div > h3').addClass('text-animated');
-   }else{
-     $('div.container.full-height.section-c > div > div > div').addClass('img-noanimated');
-     $('div.container.full-height.section-c > div > div > div').removeClass('img-animated');
-     $('div.container.full-height.section-c > div > div > div > div.see-button-wrapper').removeClass('see-animated');
-     $('div.container.full-height.section-c > div > div > div > h3').removeClass('text-animated');
-   }
-
- });
+});
 });
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
